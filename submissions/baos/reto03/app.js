cf458df51d2fc4ee62f90c794f45b9488ec08c6c
Bryan <garbray@garbray.local>
2012-11-25 16:00:14 -0500

    fixed errors and change the styles

--- submissions/baos/reto03/app.js
+++ submissions/baos/reto03/app.js
@@ -12,39 +12,39 @@ var express = require('express'),
 *App.
 */
 
-var app =  express();
-var server = http.createServer(app);
+var app =  express(),
+    server = http.createServer(app);
 
-/** 
+/**
 * App configuration
 */
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(stylus.middleware({ 
-    src: __dirname + '/stylus',
-    dest: __dirname + '/public',
-    compile: function (str, path) {
-      return stylus(str)
-        .set('filename', path)
-        .set('compress', true)
-        .use(nib())
-        .import('nib');
-      }
-  }));
-  //app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(stylus.middleware({
+        src: __dirname + '/stylus',
+        dest: __dirname + '/public',
+        compile: function (str, path) {
+            return stylus(str)
+                .set('filename', path)
+                .set('compress', true)
+                .use(nib())
+                .import('nib');
+        }
+    }));
+    //app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 // app.configure('development', function(){
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 // });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler());
 });
 
 
@@ -55,43 +55,50 @@ app.configure('production', function(){
 
 var io = require('socket.io').listen(server),
     votes = {
-      'first':0,
-      'second':0,
-      'third':0,
-      'four':0,
-      'five':0,
-      'six': 0
-    };
+        'first':0,
+        'second':0,
+        'third':0,
+        'four':0,
+        'five':0,
+        'six': 0
+    },
+    songs = {
+        'Drop of whisper': ['first','Drop of whisper', 'https://github.com/BogotaJS/bday2012/raw/master/songs/Antony_Raijekov_-_04_-_Drop_of_whisper_2005.mp3'],
+        'Lightout': ['second','Lightout','https://github.com/BogotaJS/bday2012/raw/master/songs/Antony_Raijekov_-_09_-_Lightout_2003.mp3'],
+        'While we walk': ['third','While we walk', 'https://github.com/BogotaJS/bday2012/raw/master/songs/Antony_Raijekov_-_11_-_While_We_Walk_2004.mp3'],
+        'By the coast': ['four','By the coast', 'https://github.com/BogotaJS/bday2012/raw/master/songs/Antony_Raijekov_-_12_-_By_the_Coast_2004.mp3'],
+        'Mdma pt 2': ['five','Mdma pt 2', 'https://github.com/BogotaJS/bday2012/raw/master/songs/Morus_Alba_-_01_-_MDMA_Pt_2.mp3'],
+        'Goodbye': ['six','Goodbye', 'https://github.com/BogotaJS/bday2012/raw/master/songs/Morus_Alba_-_02_-_Goodbye.mp3']
+  };
 
 
 
 io.sockets.on('connection', function (socket) {
+    /**
+    *
+    **/
+    io.sockets.emit('connected',votes);
+    /**
+    *add vote to a song
+    **/
+    socket.on('vote', function (song_id) {
+      votes[song_id] += 1;
+      io.sockets.emit('setvote',{votes: votes[song_id], id: song_id});
+    });
 
-  /**
-  *implementation nicksnames of the clients
-  **/
-  socket.on('vote', function (song_id) {
-    votes[song_id] += 1;
-    console.log('laal');
-    console.log(votes[song_id]);
-    io.sockets.emit('setvote',{votes: votes[song_id], id: song_id});
-  });
-
-  /**
-  *when the client disconnect
-  **/
-
-  socket.on('getvote', function () {
-    if (!socket.nickname) return;
-
-    delete nicknames[socket.nickname];
-    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-    socket.broadcast.emit('nicknames', nicknames);
-  });
-  var i, len = votes.length;
-  socket.on('get_song', function(){
-    
-  });
+    /**
+    *send the url of the song selected
+    **/
+    socket.on('finish_votations',function(most_voted_id){
+        console.log('finish');
+        for(var key in songs){
+            if(songs[key][0] === most_voted_id){
+                socket.emit('update_song',songs[key][2]);
+                io.sockets.emit('reset');
+                break;
+            }
+        }
+    });
 
 });
 
@@ -100,6 +107,7 @@ io.sockets.on('connection', function (socket) {
 * App routes.
 */
 app.get('/', routes.index);
+app.get('/vote', routes.vote);
 
 server.listen(3000);
 var addr = server.address();
--- submissions/baos/reto03/public/js/main.js
+++ submissions/baos/reto03/public/js/main.js
@@ -4,74 +4,128 @@
  ************************************/
 
 window.$ = function (){
-    return document.getElementById.apply(document, arguments)
-}
+    return document.getElementById.apply(document, arguments);
+};
 window.$$ = function (){
-    return document.querySelectorAll.apply(document, arguments)
-}
+    return document.querySelectorAll.apply(document, arguments);
+};
 NodeList.prototype.forEach = function(){
     return Array.prototype.forEach.apply(this, arguments);
 };
 
 Element.prototype.hasClassName = function(name) {
-  return new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)").test(this.className);
+    return new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)").test(this.className);
 };
 Element.prototype.addClassName = function(name) {
-  if (!this.hasClassName(name)) {
-    this.className = this.className ? [this.className, name].join(' ') : name;
-  }
+    if (!this.hasClassName(name)) {
+        this.className = this.className ? [this.className, name].join(' ') : name;
+    }
 };
 Element.prototype.removeClassName = function(name) {
-  if (this.hasClassName(name)) {
-    var c = this.className;
-    this.className = c.replace(new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)", "g"), "");
-  }
+    if (this.hasClassName(name)) {
+        var c = this.className;
+        this.className = c.replace(new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)", "g"), "");
+    }
 };
 /******************************
 * Global variable
 *******************************/
 
 var socket = io.connect(),
-	vote = $$('.vote');
-	sendNick = $('sendNick'),
-	nick = $('nick'),
-	text = $('message'),
-	area = $('messages'),
-  nicknames = $('nicknames'),
-  textList = '';
+    list = $('list'),
+    voted = false,
+    audio = $('song_player'),
+    most_voted = 0,
+    most_voted_id = '';
+
 
 /******************************
 * Events listeners emits socketio
 *******************************/
+if(list !== null ){
 
+    list.addEventListener('click', function(event) {
 
-    document.getElementById('list').addEventListener('click', function(event) {
-        if(event.target.tagName == 'LI') {
-
-          socket.emit('vote',event.target.getAttribute('id'));
+        if((event.target.tagName == 'P' || event.target.tagName == 'I') && voted === false) {
+            socket.emit('vote',event.target.parentNode.getAttribute('id'));
+            var el = $(event.target.parentNode.getAttribute('id'));
+            el.querySelector('i').addClassName('selected');
+            //voted = true;
+        }else{
+            //alert('you all ready vote, try when the current song finished');
         }
     });
+}
+
 
 /******************************
-* Socket.io on 
+* Socket.io on
 *******************************/
 
-socket.on('setvote', function(obj){
-  var id = obj.id,
-      votes = obj.votes;
-  $(id).querySelector('p span').innerHTML=votes;
+socket.on('connected',function(obj){
+    for(var key in obj){
+        console.log(key+' '+obj[key]);
+       $(key).querySelector('p span').innerHTML=obj[key];
+    }
 });
 
+socket.on('setvote', function(obj){
+    var id = obj.id,
+        votes = obj.votes,
+        el = $(id);
+    el.querySelector('p span').innerHTML=votes;
+});
+
+socket.on('update_song',function(url){
+    console.log(url);
+    if(audio !== null){
+        console.log('lalala');
+        change_song(url);
+    }
+});
+
+socket.on('reset',function(){
+    $$('#list_player li').forEach(function(el){
+       el.querySelector('p span').innerHTML=0;
+    });
+});
 
 /******************************
 * songs change
 *******************************/
-function change_song(){
-
-  
+function change_song(src_song){
+    audio.src = src_song;
 }
 
-video.onended = function(){
-  socket.emit('get_song');
-};
+if(audio !== null){
+    audio.addEventListener('ended', function(){
+        $$('#list_player li').forEach(function(el){
+            if(most_voted < el.querySelector('p span').textContent){
+                most_voted = el.querySelector('p span').textContent;
+                most_voted_id = el.getAttribute('id');
+            }
+        });
+        socket.emit('finish_votations', most_voted_id);
+    });
+}
+/******************************
+* sort ul
+*******************************/
 
+function sortList(listId){
+    var oUl = document.getElementById(listId);
+    for(var i in oUl.childNodes){
+        var x = oUl.childNodes[i];
+        for(var j in oUl.childNodes){
+            var y = oUl.childNodes[j];
+            // if((x.innerText != 'undefined' || y.innerText != 'undefined')  && x.innerText>y.innerText){
+            //     // Skip if x is already the first list item
+            //       if(oUl.firstChild!=x)
+            //           oUl.insertBefore(y,x);
+            //   }
+          }
+      }
+          
+  }
+
+// sortList('list');
